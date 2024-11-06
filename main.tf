9adc72f59b0dccd4297dbf3a34bf95b237594034
ShashiIOS <shashi799@gmail.com>
2024-11-06 11:10:31 +0530

    ghansyam ka rg

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "sj-rg" {
   name     = "s-rg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "gs-rg" {
+  name     = "ghansyam-rg"
+  location = "central us"
+}
